1d7bb1750d9d10ebfec9f2417f890b6a98d676bf
eshun <78552423@qq.com>
2018-08-15 16:49:26 +0800

    import element

---
+++ src/views/home/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="wrapper">
+      <h1>{{ msg }}</h1>
+    </div>
+    <!-- 创建图容器 -->
+
+  </div>
+</template>
+
+<script>
+
+    export default {
+      name: "home",
+      data() {
+        return {
+          msg: 'Welcome to Your Vue.js App'
+        }
+      },
+      mounted() {
+
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
