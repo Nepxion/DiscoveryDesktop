9962bca7d8a5bed1ac3b69db885166018797e8fa
78552423@qq.com <qq78552423>
2018-08-16 01:00:43 +0800

    layout

--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'normalize.css'
+import 'element-ui/lib/theme-chalk/index.css';
+import '@/assets/iconfont/iconfont.css'
+// global css
+import '@/styles/index.scss'
 
 Vue.use(ElementUI)
 
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
     <div class="wrapper">
-      <h1>{{ msg }}</h1>
+      <el-container>
+        <el-header height="42px">
+          <el-row>
+            <i class="el-iconfont-ecs"></i>
+            <span class="separator"></span>
+            <span class="toolbar">
+              <i class="el-iconfont-fullscreen"></i>
+            </span>
+          </el-row>
+        </el-header>
+        <el-main>{{ msg }}</el-main>
+      </el-container>
     </div>
     <!-- 创建图容器 -->
 
@@ -10,19 +21,26 @@
 
 <script>
 
-    export default {
-      name: "home",
-      data() {
-        return {
-          msg: 'Welcome to Your Vue.js App'
-        }
-      },
-      mounted() {
-
+  export default {
+    name: "home",
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App'
       }
+    },
+    mounted() {
+
     }
+  }
 </script>
 
 <style scoped>
+  .separator {
+    margin: 4px;
+    border-left: 1px solid #E9E9E9;
+  }
 
+  .toolbar {
+    float: right;
+  }
 </style>
