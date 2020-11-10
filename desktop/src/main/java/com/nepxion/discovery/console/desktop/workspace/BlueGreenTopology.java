54ef32b2ad485fb02bda9659b4c05f0438045df7
HaojunRen <1394997@qq.com>
2020-11-10 11:44:10 +0800

    重构界面

--- desktop/src/main/java/com/nepxion/discovery/console/desktop/workspace/BlueGreenTopology.java
+++ desktop/src/main/java/com/nepxion/discovery/console/desktop/workspace/BlueGreenTopology.java
@@ -13,19 +13,30 @@ import twaver.Generator;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 
+import org.apache.commons.collections4.CollectionUtils;
+
 import com.nepxion.cots.twaver.graph.TGraphBackground;
+import com.nepxion.discovery.common.constant.DiscoveryConstant;
 import com.nepxion.discovery.console.desktop.icon.ConsoleIconFactory;
 import com.nepxion.discovery.console.desktop.workspace.topology.AbstractTopology;
+import com.nepxion.swing.action.JSecurityAction;
 import com.nepxion.swing.button.JClassicButton;
 import com.nepxion.swing.combobox.JBasicComboBox;
+import com.nepxion.swing.element.ElementNode;
+import com.nepxion.swing.handle.HandleManager;
+import com.nepxion.swing.icon.IconFactory;
 import com.nepxion.swing.label.JBasicLabel;
 import com.nepxion.swing.layout.filed.FiledLayout;
+import com.nepxion.swing.selector.checkbox.JCheckBoxSelector;
 import com.nepxion.swing.textfield.JBasicTextField;
 
 public class BlueGreenTopology extends AbstractTopology {
@@ -34,9 +45,10 @@ public class BlueGreenTopology extends AbstractTopology {
     private TGraphBackground background;
 
     private JBasicComboBox serviceComboBox;
-    private JBasicComboBox blueMetadataComboBox;
-    private JBasicComboBox greenMetadataComboBox;
-    private JBasicComboBox basicMetadataComboBox;
+
+    private JBasicTextField blueMetadataTextField;
+    private JBasicTextField greenMetadataTextField;
+    private JBasicTextField defaultMetadataTextField;
 
     private JBasicTextField blueConditionTextField;
     private JBasicTextField greenConditionTextField;
@@ -50,60 +62,74 @@ public class BlueGreenTopology extends AbstractTopology {
     }
 
     private void initializeTopToolBar() {
-        serviceComboBox = new JBasicComboBox();
+        String[] services = new String[] { "discovery-guide-serivice-a" };
+        serviceComboBox = new JBasicComboBox(services);
         serviceComboBox.setPreferredSize(new Dimension(250, serviceComboBox.getPreferredSize().height));
 
-        blueMetadataComboBox = new JBasicComboBox();
-        blueMetadataComboBox.setPreferredSize(new Dimension(150, blueMetadataComboBox.getPreferredSize().height));
+        blueMetadataTextField = new JBasicTextField();
+        blueMetadataTextField.setPreferredSize(new Dimension(150, blueMetadataTextField.getPreferredSize().height));
+        JClassicButton blueMetadataButton = new JClassicButton(createMetadataSelectorAction(blueMetadataTextField));
+        blueMetadataButton.setPreferredSize(new Dimension(30, blueMetadataButton.getPreferredSize().height));
 
-        greenMetadataComboBox = new JBasicComboBox();
-        greenMetadataComboBox.setPreferredSize(new Dimension(150, greenMetadataComboBox.getPreferredSize().height));
+        greenMetadataTextField = new JBasicTextField();
+        greenMetadataTextField.setPreferredSize(new Dimension(150, greenMetadataTextField.getPreferredSize().height));
+        JClassicButton greenMetadataButton = new JClassicButton(createMetadataSelectorAction(greenMetadataTextField));
+        greenMetadataButton.setPreferredSize(new Dimension(30, greenMetadataButton.getPreferredSize().height));
 
-        basicMetadataComboBox = new JBasicComboBox();
-        basicMetadataComboBox.setPreferredSize(new Dimension(150, basicMetadataComboBox.getPreferredSize().height));
+        defaultMetadataTextField = new JBasicTextField();
+        defaultMetadataTextField.setPreferredSize(new Dimension(150, defaultMetadataTextField.getPreferredSize().height));
+        JClassicButton defaultMetadataButton = new JClassicButton(createMetadataSelectorAction(defaultMetadataTextField));
+        defaultMetadataButton.setPreferredSize(new Dimension(30, defaultMetadataButton.getPreferredSize().height));
 
         JPanel servicePanel = new JPanel();
-        servicePanel.setLayout(new FiledLayout(FiledLayout.ROW, FiledLayout.FULL, 5));
+        servicePanel.setLayout(new FiledLayout(FiledLayout.ROW, FiledLayout.FULL, 0));
         servicePanel.add(new JBasicLabel("蓝绿服务"));
         servicePanel.add(Box.createHorizontalStrut(5));
+        servicePanel.add(new JBasicLabel(ConsoleIconFactory.getSwingIcon("information_message.png")));
+        servicePanel.add(Box.createHorizontalStrut(10));
         servicePanel.add(serviceComboBox);
-        servicePanel.add(Box.createHorizontalStrut(5));
+        servicePanel.add(Box.createHorizontalStrut(10));
         servicePanel.add(new JBasicLabel("蓝版本"));
-        servicePanel.add(Box.createHorizontalStrut(5));
-        servicePanel.add(blueMetadataComboBox);
-        servicePanel.add(Box.createHorizontalStrut(5));
+        servicePanel.add(Box.createHorizontalStrut(10));
+        servicePanel.add(blueMetadataTextField);
+        servicePanel.add(blueMetadataButton);
+        servicePanel.add(Box.createHorizontalStrut(10));
         servicePanel.add(new JBasicLabel("绿版本"));
-        servicePanel.add(Box.createHorizontalStrut(5));
-        servicePanel.add(greenMetadataComboBox);
-        servicePanel.add(Box.createHorizontalStrut(5));
+        servicePanel.add(Box.createHorizontalStrut(10));
+        servicePanel.add(greenMetadataTextField);
+        servicePanel.add(greenMetadataButton);
+        servicePanel.add(Box.createHorizontalStrut(10));
         servicePanel.add(new JBasicLabel("兜底版本"));
-        servicePanel.add(Box.createHorizontalStrut(5));
-        servicePanel.add(basicMetadataComboBox);
-        servicePanel.add(Box.createHorizontalStrut(5));
+        servicePanel.add(Box.createHorizontalStrut(10));
+        servicePanel.add(defaultMetadataTextField);
+        servicePanel.add(defaultMetadataButton);
+        servicePanel.add(Box.createHorizontalStrut(10));
         servicePanel.add(new JClassicButton("添加", ConsoleIconFactory.getSwingIcon("add.png")));
         servicePanel.add(new JClassicButton("删除", ConsoleIconFactory.getSwingIcon("delete.png")));
         servicePanel.add(new JClassicButton("修改", ConsoleIconFactory.getSwingIcon("paste.png")));
 
-        blueConditionTextField = new JBasicTextField();
-        blueConditionTextField.setPreferredSize(new Dimension(398, blueConditionTextField.getPreferredSize().height));
+        blueConditionTextField = new JBasicTextField("#H['a'] == '1' && #H['b'] <= '2'");
+        blueConditionTextField.setPreferredSize(new Dimension(436, blueConditionTextField.getPreferredSize().height));
 
-        greenConditionTextField = new JBasicTextField();
-        greenConditionTextField.setPreferredSize(new Dimension(399, greenConditionTextField.getPreferredSize().height));
+        greenConditionTextField = new JBasicTextField("#H['a'] == '3'");
+        greenConditionTextField.setPreferredSize(new Dimension(436, greenConditionTextField.getPreferredSize().height));
 
         JPanel conditionPanel = new JPanel();
-        conditionPanel.setLayout(new FiledLayout(FiledLayout.ROW, FiledLayout.FULL, 5));
+        conditionPanel.setLayout(new FiledLayout(FiledLayout.ROW, FiledLayout.FULL, 0));
         conditionPanel.add(new JBasicLabel("蓝绿条件"));
         conditionPanel.add(Box.createHorizontalStrut(5));
+        conditionPanel.add(new JBasicLabel(ConsoleIconFactory.getSwingIcon("information_message.png")));
+        conditionPanel.add(Box.createHorizontalStrut(10));
         conditionPanel.add(new JBasicLabel("蓝条件"));
-        conditionPanel.add(Box.createHorizontalStrut(5));
+        conditionPanel.add(Box.createHorizontalStrut(10));
         conditionPanel.add(blueConditionTextField);
-        conditionPanel.add(Box.createHorizontalStrut(5));
+        conditionPanel.add(Box.createHorizontalStrut(10));
         conditionPanel.add(new JBasicLabel("绿条件"));
-        conditionPanel.add(Box.createHorizontalStrut(5));
+        conditionPanel.add(Box.createHorizontalStrut(10));
         conditionPanel.add(greenConditionTextField);
-        conditionPanel.add(Box.createHorizontalStrut(5));
+        conditionPanel.add(Box.createHorizontalStrut(10));
         conditionPanel.add(new JClassicButton("校验", ConsoleIconFactory.getSwingIcon("config.png")));
-        conditionPanel.add(new JClassicButton("应用", ConsoleIconFactory.getSwingIcon("save.png")));
+        conditionPanel.add(new JClassicButton("保存", ConsoleIconFactory.getSwingIcon("save.png")));
 
         JPanel toolBar = new JPanel();
         toolBar.setLayout(new FiledLayout(FiledLayout.COLUMN, FiledLayout.FULL, 5));
@@ -135,11 +161,55 @@ public class BlueGreenTopology extends AbstractTopology {
 
     private void initializeTopology() {
         background = graph.getGraphBackground();
-        background.setTitle("蓝绿部署 | 版本策略 | 局部订阅模式");
+        background.setTitle("My Blue Green | 蓝绿部署 | 版本策略 | 局部订阅模式");
         graph.setElementStateOutlineColorGenerator(new Generator() {
             public Object generate(Object object) {
                 return null;
             }
         });
+    }
+
+    private JSecurityAction createMetadataSelectorAction(JBasicTextField metadataTextField) {
+        JSecurityAction action = new JSecurityAction(ConsoleIconFactory.getSwingIcon("direction_south.png"), "版本选取") {
+            private static final long serialVersionUID = 1L;
+
+            @SuppressWarnings("unchecked")
+            public void execute(ActionEvent e) {
+                String[] metadatas = new String[] { "20201110-001", "20201110-002", "20201110-003", "default" };
+
+                List<ElementNode> metadataElementNodes = new ArrayList<ElementNode>();
+                for (String metadata : metadatas) {
+                    metadataElementNodes.add(new ElementNode(metadata, IconFactory.getSwingIcon("component/file_chooser_16.png"), metadata, metadata));
+                }
+
+                JCheckBoxSelector checkBoxSelector = new JCheckBoxSelector(HandleManager.getFrame(BlueGreenTopology.this), "版本选取", new Dimension(400, 350), metadataElementNodes);
+                checkBoxSelector.setVisible(true);
+                checkBoxSelector.dispose();
+
+                if (!checkBoxSelector.isConfirmed()) {
+                    return;
+                }
+
+                List<String> selectedMetadatas = checkBoxSelector.getSelectedUserObjects();
+                if (CollectionUtils.isEmpty(selectedMetadatas)) {
+                    return;
+                }
+
+                StringBuilder StringBuilder = new StringBuilder();
+                int index = 0;
+                for (String selectedMetadata : selectedMetadatas) {
+                    StringBuilder.append(selectedMetadata);
+                    if (index < selectedMetadatas.size() - 1) {
+                        StringBuilder.append(DiscoveryConstant.SEPARATE);
+                    }
+
+                    index++;
+                }
+
+                metadataTextField.setText(StringBuilder.toString());
+            }
+        };
+
+        return action;
     }
 }
