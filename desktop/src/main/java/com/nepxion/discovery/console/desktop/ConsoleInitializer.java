8baaa3f0149f6afdedb735b40ae539a791f703b3
HaojunRen <1394997@qq.com>
2020-11-18 09:08:10 +0800

    重构界面

--- desktop/src/main/java/com/nepxion/discovery/console/desktop/ConsoleInitializer.java
+++ desktop/src/main/java/com/nepxion/discovery/console/desktop/ConsoleInitializer.java
@@ -9,11 +9,18 @@ package com.nepxion.discovery.console.desktop;
  * @version 1.0
  */
 
+import com.nepxion.banner.BannerConstant;
+import com.nepxion.banner.Description;
+import com.nepxion.banner.LogoBanner;
+import com.nepxion.banner.NepxionBanner;
+import com.nepxion.discovery.common.constant.DiscoveryConstant;
+import com.nepxion.discovery.console.desktop.common.context.ConsoleBannerContext;
 import com.nepxion.discovery.console.desktop.common.context.ConsoleDataContext;
 import com.nepxion.discovery.console.desktop.common.context.ConsolePropertiesContext;
 import com.nepxion.discovery.console.desktop.common.context.ConsoleUIContext;
 import com.nepxion.discovery.console.desktop.common.icon.ConsoleIconFactory;
 import com.nepxion.discovery.console.desktop.common.locale.ConsoleLocaleFactory;
+import com.taobao.text.Color;
 
 public class ConsoleInitializer {
     private static final String PROPERTIES_PATH = "config/console.properties";
@@ -21,6 +28,7 @@ public class ConsoleInitializer {
     private static final String LOCALE_PATH = "com/nepxion/discovery/console/desktop/common/locale/";
 
     public static void initialize() {
+        ConsoleBannerContext.initialize();
         ConsolePropertiesContext.initialize(PROPERTIES_PATH);
         ConsoleDataContext.initialize();
         ConsoleUIContext.initialize();
