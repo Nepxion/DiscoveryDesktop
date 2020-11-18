4ef1b6607636dd31c884bb689a972de2efba8f8c
HaojunRen <1394997@qq.com>
2020-11-18 21:33:37 +0800

    重构界面

--- desktop/src/main/java/com/nepxion/discovery/console/desktop/ConsoleInitializer.java
+++ desktop/src/main/java/com/nepxion/discovery/console/desktop/ConsoleInitializer.java
@@ -9,18 +9,12 @@ package com.nepxion.discovery.console.desktop;
  * @version 1.0
  */
 
-import com.nepxion.banner.BannerConstant;
-import com.nepxion.banner.Description;
-import com.nepxion.banner.LogoBanner;
-import com.nepxion.banner.NepxionBanner;
-import com.nepxion.discovery.common.constant.DiscoveryConstant;
 import com.nepxion.discovery.console.desktop.common.context.ConsoleBannerContext;
 import com.nepxion.discovery.console.desktop.common.context.ConsoleDataContext;
 import com.nepxion.discovery.console.desktop.common.context.ConsolePropertiesContext;
 import com.nepxion.discovery.console.desktop.common.context.ConsoleUIContext;
 import com.nepxion.discovery.console.desktop.common.icon.ConsoleIconFactory;
 import com.nepxion.discovery.console.desktop.common.locale.ConsoleLocaleFactory;
-import com.taobao.text.Color;
 
 public class ConsoleInitializer {
     private static final String PROPERTIES_PATH = "config/console.properties";
