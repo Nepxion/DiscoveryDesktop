e0e98b29b69124f3e95cd194fc10dee5b247717c
HaojunRen <1394997@qq.com>
2020-11-24 11:54:55 +0800

    重构界面

--- desktop/src/main/java/com/nepxion/discovery/console/desktop/workspace/AbstractStrategyTopology.java
+++ desktop/src/main/java/com/nepxion/discovery/console/desktop/workspace/AbstractStrategyTopology.java
@@ -1,5 +1,14 @@
 package com.nepxion.discovery.console.desktop.workspace;
 
+/**
+ * <p>Title: Nepxion Discovery</p>
+ * <p>Description: Nepxion Discovery</p>
+ * <p>Copyright: Copyright (c) 2017-2050</p>
+ * <p>Company: Nepxion</p>
+ * @author Haojun Ren
+ * @version 1.0
+ */
+
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.util.ArrayList;
