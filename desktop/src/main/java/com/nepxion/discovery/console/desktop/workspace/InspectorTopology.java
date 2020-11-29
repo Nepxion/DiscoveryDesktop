f6dd70a17bb0b4e718d7f099dc7843b145822a94
HaojunRen <1394997@qq.com>
2020-11-29 11:14:42 +0800

    重构界面

--- desktop/src/main/java/com/nepxion/discovery/console/desktop/workspace/InspectorTopology.java
+++ desktop/src/main/java/com/nepxion/discovery/console/desktop/workspace/InspectorTopology.java
@@ -229,7 +229,7 @@ public class InspectorTopology extends AbstractTopology {
 
         dimensionComboBox = new JBasicComboBox(dimensionElementNodes.toArray());
 
-        Integer[] times = new Integer[] { 1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000 };
+        Integer[] times = new Integer[] { 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000 };
         timesComboBox = new JBasicComboBox(times);
 
         progressBar = new JProgressBar();
